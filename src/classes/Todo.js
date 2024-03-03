5078af67d737ef14d54d0921ad5d65678dc2a7c7
kz <kennethlorenzg@gmail.com>
2024-03-02 23:51:45 -0500

    Add todo class

---
+++ src/classes/Todo.js
@@ -0,0 +1,8 @@
+export class Todo {
+  constructor(title, details, dueDate, priority) {
+    this.title = title;
+    this.details = details;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
