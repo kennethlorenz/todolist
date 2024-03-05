13c69984f8c2fdd1a2ce3f115d471db163ab46cc
kz <kennethlorenzg@gmail.com>
2024-03-05 18:26:29 -0500

    Add AddNewProject function when user clicks Create Project

--- src/ProjectForm.js
+++ src/ProjectForm.js
@@ -1,20 +1,31 @@
-export default function CreateProject() {
-  const projectForm = document.createElement("form");
-  projectForm.id = "addProjectForm";
-  const titleTextArea = document.createElement("textarea");
-  titleTextArea.id = "projectName";
-  titleTextArea.placeholder = "Project Name: House Renovations";
-  titleTextArea.name = "projectName";
+import CreateProject from "./CreateProject";
+const projectForm = document.createElement("form");
+projectForm.id = "addProjectForm";
+projectForm.method = "post";
+const titleTextArea = document.createElement("textarea");
+titleTextArea.id = "projectName";
+titleTextArea.placeholder = "Project Name: House Renovations";
+titleTextArea.name = "projectName";
 
-  const submitBtn = document.createElement("button");
-  submitBtn.type = "submit";
-  submitBtn.id = "addTodo";
-  submitBtn.textContent = "Create Project";
+const submitBtn = document.createElement("button");
+submitBtn.type = "submit";
+submitBtn.id = "addProject";
+submitBtn.textContent = "Create Project";
 
-  projectForm.appendChild(titleTextArea);
-  projectForm.appendChild(submitBtn);
-  const div = document.createElement("div");
-  div.textContent = "create project page";
+projectForm.appendChild(titleTextArea);
+projectForm.appendChild(submitBtn);
+const div = document.createElement("div");
+div.textContent = "create project page";
 
+export function CreateNewProjectForm() {
   return projectForm;
+}
+
+export function AddNewProject() {
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = titleTextArea.value;
+    CreateProject(title);
+    titleTextArea.value = "";
+  });
 }
