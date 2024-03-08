ffb5c07584cc5018b07cff38c5915de31a89b800
kz <kennethlorenzg@gmail.com>
2024-03-07 22:56:05 -0500

    Moved modal dom manipulation to createmodaldom

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import CreateModalDOM from "./CreateModalDOM";
 import { AddNewProject } from "./ProjectForm";
 import SideBarDom from "./SidebarDOM";
-const addButton = document.getElementById("add");
-const modal = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector(".close");
-const modalContent = document.querySelector(".modal-content");
-
-addButton.addEventListener("click", () => {
-  modal.style.display = "unset";
-  modalContent.classList.add("modalZoom");
-  modalContent.classList.remove("modalZoomOut");
-});
-
-modalCloseBtn.addEventListener("click", () => {
-  closeModal();
-});
-
-function closeModal() {
-  modalContent.classList.remove("modalZoom");
-  modalContent.classList.add("modalZoomOut");
-  setTimeout(() => {
-    modal.style.display = "none";
-  }, 200);
-  resetForm();
-}
 
 CreateModalDOM();
 AddNewProject();
