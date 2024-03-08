3a6a2eb3bed11cc5a3e0f4f94046cf1c189fe72f
kz <kennethlorenzg@gmail.com>
2024-03-07 22:38:44 -0500

    Add array to return

--- src/CreateProject.js
+++ src/CreateProject.js
@@ -1,16 +1,18 @@
 import ProjectClass from "./classes/ProjectClass";
-import project from "./project";
 const allKeys = Object.keys(localStorage);
 export default function CreateProject(projectName) {
   const newProject = new ProjectClass(projectName);
-
   localStorage.setItem(projectName, JSON.stringify(newProject));
 }
 
 export function GetProjects() {
-  return allKeys.map((key) => {
-    console.log(JSON.parse(localStorage.getItem(key)));
+  const projects = [];
+  allKeys.map((key) => {
+    const item = JSON.parse(localStorage.getItem(key));
+    projects.push(item);
   });
+
+  return projects;
 }
 
 export function ProjectExists(projectName) {
