09488f485d82e7553bd5399f226053ad379cf0da
kz <kennethlorenzg@gmail.com>
2024-03-15 23:06:16 -0400

    Add textContent and fix classlist

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,11 @@
-export default function todoItem(todoTitle) {
+export default function todoItem(
+  projectName,
+  title,
+  details,
+  dueDate,
+  priority,
+  checked
+) {
   const container = document.createElement("div");
   container.classList.add("todoContainer");
 
@@ -8,9 +15,10 @@ export default function todoItem(todoTitle) {
   const todoCheckbox = document.createElement("input");
   todoCheckbox.type = "checkbox";
   todoCheckbox.name = "todo";
+  todoCheckbox.checked = checked;
   const todoLabel = document.createElement("label");
   todoLabel.htmlFor = "todo";
-  todoLabel.textContent = todoTitle;
+  todoLabel.textContent = title;
 
   firstSection.appendChild(todoCheckbox);
   firstSection.appendChild(todoLabel);
@@ -22,12 +30,16 @@ export default function todoItem(todoTitle) {
 
   const detailsButton = document.createElement("button");
   detailsButton.classList.add("todo-details");
+  detailsButton.textContent = "Details";
   const todoDueDate = document.createElement("p");
   todoDueDate.classList.add("todo-duedate");
+  todoDueDate.textContent = dueDate;
   const editTodoButton = document.createElement("i");
-  editTodoButton.classList.add("fa-regular fa-pen-to-square");
+  editTodoButton.classList.add("fa-regular");
+  editTodoButton.classList.add("fa-pen-to-square");
   const deleteTodoButton = document.createElement("i");
-  deleteTodoButton.classList.add("fa-solid fa-trash");
+  deleteTodoButton.classList.add("fa-solid");
+  deleteTodoButton.classList.add("fa-trash");
 
   secondSection.appendChild(detailsButton);
   secondSection.appendChild(todoDueDate);
