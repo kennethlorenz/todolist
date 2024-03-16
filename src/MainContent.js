475e62b7d4aa0ef7bc581098cf31a93f890e98f7
kz <kennethlorenzg@gmail.com>
2024-03-15 23:01:21 -0400

    Add renderAllTodos Function
    Goes through todos of selected project and creates a TodoItem
    to be appended through todos container.
    
    Invoke same function on renderMainContent.

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,3 +1,6 @@
+import { getTodosFromSelectedProject } from "./LocalStorageManager";
+import todoItem from "./TodoItem";
+import { initializeHomeStorage } from "./LocalStorageManager";
 export default function mainContent(projectName) {
   const mainDiv = document.createElement("div");
   mainDiv.classList.add("main");
@@ -20,6 +23,36 @@ export default function mainContent(projectName) {
 
 export function renderMainContent(project) {
   let body = document.querySelector("body");
-  body.appendChild(mainContent(project.dataset.id), body.children[3]);
+  body.replaceChild(mainContent(project.dataset.id), body.children[3]);
   //mainDiv.replaceChildren(MainContent(project.dataset.id));
+  renderAllTodos();
+}
+
+export function renderAllTodos() {
+  const key = document.querySelector(".project.selected").dataset.id;
+  let projectTodos;
+
+  const todosContainer = document.querySelector("#todosContainer");
+
+  if (getTodosFromSelectedProject(key) == null) {
+    return;
+  } else {
+    projectTodos = getTodosFromSelectedProject(key);
+  }
+  //const projectTodos = getTodosFromSelectedProject(key);
+  projectTodos.forEach((item) => {
+    console.log(
+      `Title: ${item.title}, Details: ${item.details}, DueDate: ${item.dueDate}, Priority: ${item.priorty}, Checked: ${item.checked} `
+    );
+
+    const todo = todoItem(
+      key,
+      item.title,
+      item.details,
+      item.dueDate,
+      item.priorty,
+      item.checked
+    );
+    todosContainer.appendChild(todo);
+  });
 }
