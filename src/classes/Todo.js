5e4381b1e8c5ce5b6e71fe1a4b482d3f8959b027
kz <kennethlorenzg@gmail.com>
2024-03-18 00:46:35 -0400

    add checked and index parameters

--- src/classes/Todo.js
+++ src/classes/Todo.js
@@ -1,9 +1,10 @@
 export default class Todo {
-  constructor(title, details, dueDate, priority) {
+  constructor(title, details, dueDate, priority, checked, index) {
     this.title = title;
     this.details = details;
     this.dueDate = dueDate;
     this.priority = priority;
-    this.checked = false;
+    this.checked = checked;
+    this.index = index;
   }
 }
