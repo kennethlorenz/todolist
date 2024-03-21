91836f38434c6492452426982134c2ee7444f25b
kz <kennethlorenzg@gmail.com>
2024-03-21 17:19:06 -0400

    Add open and close modal function

--- src/DetailsScreen.js
+++ src/DetailsScreen.js
@@ -1,22 +1,23 @@
-const detailsCloseButton = document.querySelector("#detailsModal .close");
+import { openModal, closeModal } from "./ModalHandler";
+
 export default function displayDetailsScreen(key, todo) {
+  const detailsCloseButton = document.querySelector("#detailsModal .close");
+  const detailsModal = document.getElementById("detailsModal");
+  const detailsContent = document.getElementById("detailsContent");
   const title = document.getElementById("todoTitle");
   const projectName = document.getElementById("projectName");
   const priority = document.getElementById("priority");
   const dueDate = document.getElementById("dueDate");
   const details = document.getElementById("details");
 
-  const detailsModal = document.getElementById("detailsModal");
-
   title.textContent = todo.title;
   projectName.textContent = key;
   priority.textContent = todo.priority;
   dueDate.textContent = todo.dueDate;
   details.textContent = todo.details;
+  openModal(detailsModal, detailsContent);
 
-  detailsModal.style.display = "unset";
+  detailsCloseButton.addEventListener("click", () => {
+    closeModal(detailsModal, detailsContent);
+  });
 }
-
-detailsCloseButton.addEventListener("click", () => {
-  detailsModal.style.display = "none";
-});
