d98561e9b7ada1e84fcdb483c231a5f1efd8b101
kz <kennethlorenzg@gmail.com>
2024-03-29 23:10:21 -0400

    remove index property

--- src/classes/Todo.js
+++ src/classes/Todo.js
@@ -1,10 +1,9 @@
 export default class Todo {
-  constructor(title, details, dueDate, priority, checked, index) {
+  constructor(title, details, dueDate, priority, checked) {
     this.title = title;
     this.details = details;
     this.dueDate = dueDate;
     this.priority = priority;
     this.checked = checked;
-    this.index = index;
   }
 }
